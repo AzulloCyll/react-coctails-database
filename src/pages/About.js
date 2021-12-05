8205ce5ace80c0856e37d64d9111b1130ce851a8
Daniel Chmur <daniel.chmur@gmail.com>
2021-12-05 13:26:50 +0100

    last

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        repellendus eius minima reprehenderit, assumenda rerum tempora nulla
+        doloremque dolore dolores recusandae voluptate ad sequi soluta omnis,
+        ipsum minus delectus accusantium.
+      </p>
+    </section>
+  );
+}
